7e695d9ebb80ec748ccc7b24c3846f660eb1866e
Heisenberg <qilei@53zaixian.com>
2024-03-22 09:31:56 +0800

    addlog

--- src/main/java/com/heisenberg/chatroom/handler/MessageHandler.java
+++ src/main/java/com/heisenberg/chatroom/handler/MessageHandler.java
@@ -1,7 +1,11 @@
 package com.heisenberg.chatroom.handler;
 
+import com.alibaba.fastjson2.JSONObject;
+import com.heisenberg.chatroom.pojo.UserInfo;
+import com.heisenberg.chatroom.proto.ChatCode;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,7 +16,27 @@ public class MessageHandler extends SimpleChannelInboundHandler<TextWebSocketFra
     private static final Logger logger = LoggerFactory.getLogger(MessageHandler.class);
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
-        UserInfoManager
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame frame) throws Exception {
+        UserInfo userInfo = UserInfoManager.getUserInfo(ctx.channel());
+        if (userInfo != null && userInfo.isAuth()) {
+            JSONObject json = JSONObject.parseObject(frame.text());
+            // 广播返回 用户发送的消息文本
+            UserInfoManager.broadcastMess(userInfo.getUserId(), userInfo.getNick(), json.getString("mess"));
+        }
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        UserInfoManager.removeChannel(ctx.channel());
+        UserInfoManager.broadCastInfo(ChatCode.SYS_USER_COUNT,UserInfoManager.getAuthUserCount());
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        logger.error("connection error and close the channel", cause);
+        UserInfoManager.removeChannel(ctx.channel());
+        UserInfoManager.broadCastInfo(ChatCode.SYS_USER_COUNT, UserInfoManager.getAuthUserCount());
+
     }
 }
